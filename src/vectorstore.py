5f92c4b54d373963fcaf68ae469dffb482837e9a
danielc149 <danielc149@gmail.com>
2026-06-26 15:09:31 -0400

    fix text splitter import for langchain compatibility

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OllamaEmbeddings
 
