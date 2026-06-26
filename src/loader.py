021a93cc51930195995c2c9cb3f118deae0ca7fc
danielc149 <danielc149@gmail.com>
2026-06-26 14:26:37 -0400

    implement pdf loader

---
+++ src/loader.py
@@ -0,0 +1,12 @@
+from langchain.document_loaders import PyPDFLoader
+import os
+
+def load_documents(data_path="data"):
+    documents = []
+    
+    for file in os.listdir(data_path):
+        if file.endswith(".pdf"):
+            loader = PyPDFLoader(os.path.join(data_path, file))
+            documents.extend(loader.load())
+    
+    return documents
