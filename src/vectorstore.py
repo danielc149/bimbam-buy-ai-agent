eda7de4bd787b5735a0726d90e45fb88def204bc
danielc149 <danielc149@gmail.com>
2026-06-26 18:14:54 -0400

    fix dependency versions for stable langchain

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -1,4 +1,4 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OllamaEmbeddings
 
