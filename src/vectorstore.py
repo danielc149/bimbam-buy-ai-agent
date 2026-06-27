1c7d2b8fc599efd198a334cf652b4eec26a0503d
danielc149 <danielc149@gmail.com>
2026-06-27 19:16:06 -0400

    allow safe FAISS deserialization

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -1,8 +1,24 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OllamaEmbeddings
+import os
 
 def create_vectorstore(documents):
+
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+    # ✅ SI YA EXISTE EL ÍNDICE → cargarlo (OCI)
+    if os.path.exists("faiss_index"):
+        print("Cargando vectorstore existente...")
+        db = FAISS.load_local(
+        "faiss_index",
+        embeddings,
+        allow_dangerous_deserialization=True
+        )
+        return db
+
+    print("Creando vectorstore nuevo...")
+
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1500,
         chunk_overlap=300
@@ -10,8 +26,9 @@ def create_vectorstore(documents):
 
     docs = splitter.split_documents(documents)
 
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
-
     db = FAISS.from_documents(docs, embeddings)
+
+    # ✅ guardamos embeddings (SOLO se genera 1 vez)
+    db.save_local("faiss_index")
 
     return db
